ea0646cee17b7695291997634e2f7fc2b8be2dfa
Zhanatt <zhanattool@gmail.com>
2023-03-02 01:13:45 +0600

    second-commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,36 +2,6 @@ import React, {useState, useEffect} from 'react'
 import HeaderTop from './HeaderTop'
 
 const Header = () => {
-    // const headerTop = document.querySelector('.header-top');
-    
-    // const [count,setCount] = useState();
-
-    // function setCount(){
-
-    //     if(window.pageYOffset > 100){
-    //         console.log(888)
-    //     }
-    //     else{
-    //         console.log(777)
-    //     }
-    // }
-    // const [offset, setOffset] = useState('header-top');
-
-    // useEffect(() => {
-    //     const onScroll = () => setOffset(window.pageYOffset);
-    //     // clean up code
-    //     window.removeEventListener('scroll', onScroll);
-    //     window.addEventListener('scroll', onScroll, { passive: true });
-    //     return () => window.removeEventListener('scroll', onScroll);
-        
-    // }, []);
-
-    // if(offset >= 700){
-    //     console.log("Zhanat")
-
-
-    // }
-    
     
   return (
     <div className='header'>
